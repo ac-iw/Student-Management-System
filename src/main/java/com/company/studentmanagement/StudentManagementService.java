746c291881a375dbf31bbee61048567542695800
Adam Chappell <adam.chappell@integration.works>
2021-12-02 16:22:58 +1300

    Repo restructuring

--- src/main/java/com/company/studentmanagement/StudentManagementService.java
+++ src/main/java/com/company/studentmanagement/StudentManagementService.java
@@ -1,13 +1,5 @@
 package com.company.studentmanagement;
-import com.company.studentmanagement.domain.Course;
-import com.company.studentmanagement.domain.FullTimeStudent;
-import com.company.studentmanagement.domain.PartTimeStudent;
-import com.company.studentmanagement.domain.Student;
 import com.company.studentmanagement.repository.StudentRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 
 public class StudentManagementService {
